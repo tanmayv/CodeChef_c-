49bbd68de5d64e5d7ed175df06cbc280f689611b
Tanmay Vijayvargiya <tanmay.vijayvargiya@sap.com>
2018-01-24 23:12:43 +0530

    Initial Commit

---
+++ CodeChef/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodeChef
+//
+//  Created by Vijayvargiya, Tanmay on 24/01/18.
+//  Copyright © 2018 Vijayvargiya, Tanmay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
